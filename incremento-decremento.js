3a25ebf0af7f78ec7979d101892b2a842fe08a97
Robson Chagas de Freitas <102165122+Robson6x6@users.noreply.github.com>
2025-06-16 10:51:13 -0300

    estudo sobre incremento e decremento.

---
+++ incremento-decremento.js
@@ -0,0 +1,38 @@
+//incremento '++'
+//utilizamos quando queremos somar um valor a uma variavel por exemplo
+
+console.log("Usando incremento:")
+
+let contador = 1
+
+console.log("Meu contador inicial é: " + contador)
+
+//pode-se usar
+contador = contador + 1
+
+console.log("Aqui estou usando 'contador = contador + 1' e o resultado foi para: " + contador)
+
+//que dará o resultado dois
+
+//pode-se usar para facilitaro contador++
+//que o resultado será o mesmo, com menos código escrito nesse caso o que começou com 1 agora está com 3
+
+contador++
+ console.log("Aqui é utilizado o 'contador++' e resultado foi atualizado para : " + contador)
+
+ //decremento '--'
+
+ //O decremento faz o inverso do anterior
+ 
+console.log("Usando decremeto:")
+
+ console.log("Meu contador está em: " + contador)
+
+ contador = contador - 1
+
+ console.log("Usando 'contador = contador -1' o valor foi para: " + contador)
+
+contador--
+
+
+console.log("Usando o 'contador--' voltou para o valor inicial que é era: " + contador)
