c3064e4cd5669aef8c81a719024b02e7a4cfe395
Robson Chagas de Freitas <102165122+Robson6x6@users.noreply.github.com>
2025-06-16 23:44:06 -0300

    estudos for  while e do while

---
+++ while.js
@@ -0,0 +1,12 @@
+//while signigica enquanto
+
+//para que não ocorra um loop infinito tempos que incluir o interado dentro do while
+
+//não podemos esquecer de implementar o while
+
+let i = 0
+
+while(i < 3){
+    console.log("olá " + i)
+    i++
+}
