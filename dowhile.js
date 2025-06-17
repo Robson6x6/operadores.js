e3ce00d57d85a2961daf45eed72accba3da8c74b
Robson Chagas de Freitas <102165122+Robson6x6@users.noreply.github.com>
2025-06-16 23:44:54 -0300

    dowhile

---
+++ dowhile.js
@@ -0,0 +1,13 @@
+//do while faça enquanto diferente do while colocamos a regra no final
+
+//aqui ele primeiro faz e depois que verifica ou seja, será executado pelo menos uma vez.
+
+// no while ele primeiro verifica depois faz.
+
+let i = 0
+
+do {
+    console.log("ola")
+    i++
+} while (i < 3)
+    
